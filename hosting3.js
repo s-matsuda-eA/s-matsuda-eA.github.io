03098e364eec3aad8525097a3cabccf3f6e69ce5
sakurako matsuda <sakurako.matsuda@dragon.jp>
2023-08-15 10:43:20 +0900

    modify

--- hosting3.js
+++ hosting3.js
@@ -1,16 +1,21 @@
 $(function () {
-  // url生成
-  const originalUrl = $("link[rel='alternate'][hreflang='ja']");
-  console.log(originalUrl.attr("href"));
-  let requestUrl = "";
+  const requestPath = "/stt-sandbox-demosite/frametest.html";
 
-  // $.ajax({
-  //   type: "GET",
-  //   url: requestUrl,
-  // })
-  //   .done(function () {})
-  //   .fail(function () {})
-  //   .always(function (result) {
-  //     console.log("結果 %o", result);
-  //   });
+  // オリジナルホストのURLを作成する
+  const alternateJa = $("link[rel='alternate'][hreflang='ja']");
+  const requestUrl = alternateJa
+    ? new URL(requestPath, alternateJa.attr("href"))
+    : new URL(requestPath, new URL(location.href));
+
+  console.log("requestURL %o", requestUrl);
+
+  $.ajax({
+    type: "GET",
+    url: requestUrl.href,
+  })
+    .done(function () {})
+    .fail(function () {})
+    .always(function (result) {
+      console.log("結果 %o", result);
+    });
 });
